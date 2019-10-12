447145d26f393f5933529b383fc112f139953237
Chenlin Liu <cl2trg@virginia.edu>
2019-10-12 12:09:43 -0400

    Fix SubscribeUs.js

--- frontend/src/components/SubscribeUs.js
+++ frontend/src/components/SubscribeUs.js
@@ -4,15 +4,20 @@ export default class SubscribeUs extends React.Component{
     render() {
         return(
             <body>
-            <h2>Subscribe to our newsletters</h2>
             <form>
-                Your Name:<br>
-                <input type="text" name="subscriberName"/>
-                </br>
-                Email Address:<br>
-                <input type="text" name="subscriberEmail"/>
-                </br>
-                <input type="submit" value="Submit"/>
+                <fieldset>
+                    <legend>Subscribe to our newsletters</legend>
+                    <p>
+                    <label>Your Name</label>
+                    <input type = "text"
+                           name = "subscriberName" />
+                    </p>
+                    <p>
+                    <label>Email Address</label>
+                    <input type = "text"
+                           name = "subscriberEmail" />
+                    </p>
+                </fieldset>
             </form>
             </body>
         );
