f5daf7ce1588be90ac515f13373d8e74400d6de8
Chenlin Liu <cl2trg@virginia.edu>
2019-12-24 11:37:13 +0800

    Changed Header.js to Navbar.js and moved header code to App.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,13 @@
 import React, {Component} from 'react';
 import {Button, Menu, Container} from 'semantic-ui-react'
-import logo from '../assets/images/cssslogo_white.png'
+
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
   Link
 } from "react-router-dom";
 
-import MainPage from '../pages/MainPage'
-import TeamPage from '../pages/TeamPage'
-import SignupPage from '../pages/SignupPage'
 
-
-class Navbar extends Component {
+export default class Navbar extends Component {
   state = {}
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
@@ -54,25 +47,3 @@ class Navbar extends Component {
   }
 }
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header className="site-navbar pt-3" role="banner">
-        <Container className="logo-container" fluid>
-            <img src={logo} className="App-logo" alt="logo" />
-        </Container>
-
-        <Router>
-          <Navbar className='App-navbar'>
-          </Navbar>
-
-          <Switch>
-            <Route exact path="/" component={MainPage}></Route>
-            <Route exact path="/team" component={TeamPage}></Route>
-            <Route exact path='/signup' component={SignupPage}></Route>
-          </Switch>
-        </Router>
-      </header>
-    );
-  }
-}
