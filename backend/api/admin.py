f79409e2c62cd7ecad6eb311b476d049f86e418e
sijuntan <sijuntan@yeah.net>
2020-01-03 17:53:34 +0800

    Enable google-oauth2

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+class MyUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('comp_id', 'username', 'password1', 'password2'),
+        }),
+    )
+
 admin.site.register(User, UserAdmin)
--- backend/api/models.py
+++ backend/api/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    comp_id = models.CharField(max_length=50, primary_key=True)
-    password = models.CharField(max_length=100, null=True)
+    #comp_id = models.CharField(max_length=50, unique=True)
+    #password = models.CharField(max_length=100, null=True)
+    pass
 
     def __str__(self):
         return self.username
